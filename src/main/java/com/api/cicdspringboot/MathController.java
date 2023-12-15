24333440be6d6a781073432d476bf1f1560a81a0
Celso Machado <celsobarrosmachado@gmail.com>
2023-12-15 13:12:20 +0000

    Starting Calculator CICD

---
+++ src/main/java/com/api/cicdspringboot/MathController.java
@@ -0,0 +1,23 @@
+package com.api.cicdspringboot;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MathController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}")
+    public Double sum (@PathVariable(value = "numberOne") String numberOne,
+                       @PathVariable(value = "numberTwo") String numberTwo){
+
+        return 1D;
+    }
+
+
+}
